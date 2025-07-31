838f40fe7b28fd80fb6eaa9ef137ad072a2f250c
saeedkamyab <saeed.kamyab@yahoo.com>
2025-07-31 19:20:21 +0330

    unit of work and base repository interface

---
+++ LMS.Application/LMS.Application/Contracts/Persistence/Common/IBaseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace LMS.Application.Contracts.Persistence.Common
+{
+    public interface IBaseRepository<T> where T:class
+    {
+        Task<T> GetByIdAsync(Guid id);
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ LMS.Application/LMS.Application/Contracts/Persistence/IUnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using LMS.Application.Contracts.Persistence.Common;
+using LMS.ApplicationCore.Entities.Education;
+using LMS.ApplicationCore.Entities.Finance;
+using LMS.ApplicationCore.Entities.Identity;
+
+namespace LMS.Application.Contracts.Persistence
+{
+    public interface IUnitOfWork
+    {
+        #region Identity
+        IBaseRepository<Teacher> TeacherRepository { get; }
+        IBaseRepository<Student> StudentRepository { get; }
+        IBaseRepository<Employee> EmployeeRepository { get; }
+        IBaseRepository<PhoneNumber> PhoneNumberRepository { get; }
+        #endregion
+
+        #region Education
+        IBaseRepository<Level> LevelRepository { get; }
+        IBaseRepository<Register> RegisterRepository { get; }
+        IBaseRepository<TermCourse> TermCourseRepository { get; }
+        IBaseRepository<TermCourseType> TermCourseTypeepository { get; }
+
+        #endregion
+       
+        #region Finance
+        IBaseRepository<PaymentTuition> PaymentTuitionRepository { get; }
+        #endregion
+        Task<int> CommitAsync();
+    }
+}
