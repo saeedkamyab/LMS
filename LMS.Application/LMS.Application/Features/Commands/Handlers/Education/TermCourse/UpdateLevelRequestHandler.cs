c97286f0827b4e2ea180249d9e70eed111804482
saeedkamyab <saeed.kamyab@yahoo.com>
2025-09-25 19:37:46 +0330

    TermCourse Request Handler

---
+++ LMS.Application/LMS.Application/Features/Commands/Handlers/Education/TermCourse/UpdateLevelRequestHandler.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using LMS.Application.Contracts.Persistence;
+using LMS.Application.Features.Commands.Requests.Education.TermCourse;
+using LMS.Application.Dtos.Education;
+using MediatR;
+using LMS.Application.Dtos.Common;
+using LMS.Application.Dtos.Education.Validations;
+using LMS.Application.Constants.Enums;
+using LMS.Application.Constants.MessageText;
+
+namespace LMS.Application.Features.Commands.Handlers.Education.TermCourse
+{
+    public class UpdateTermCourseRequestHandler : IRequestHandler<UpdateTermCourseRequest, BaseCommandResponse>
+    {
+        IUnitOfWork _unitOfWork;
+        IMapper _mapper;
+        TermCourseValidator _validator;
+        public UpdateTermCourseRequestHandler(IUnitOfWork unitOfWork, IMapper mapper, TermCourseValidator validator)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _validator = validator;
+        }
+        public async Task<BaseCommandResponse> Handle(UpdateTermCourseRequest request, CancellationToken cancellationToken)
+        {
+
+            var validationResult = await _validator.ValidateAsync(request.TermCourseDto, cancellationToken);
+            if(!validationResult.IsValid)
+            {
+                return new BaseCommandResponse()
+                {
+                    Status = ResponseStatusCodes.ValidationError,
+                    Message = ResponseMessageText.FAILD_SAVING,
+                    Errors = validationResult.Errors.Select(e => e.ErrorMessage).ToList()
+                };
+            }
+            try
+            {
+                var TermCourse = _mapper.Map<LMS.ApplicationCore.Entities.Education.TermCourse>(request.TermCourseDto);
+                await _unitOfWork.TermCourseRepository.UpdateAsync(TermCourse);
+                return new BaseCommandResponse
+                {
+                    Status = ResponseStatusCodes.Success,
+                    Message = ResponseMessageText.SUCCESSFUL_SAVING,
+                };
+            }
+            catch(Exception ex)
+            {
+                return new BaseCommandResponse
+                {
+                    Status = ResponseStatusCodes.Faild,
+                    Message = ResponseMessageText.FAILD_SAVING,
+                    Errors = new List<string> { ex.Message }
+                };
+            }
+        }
+    }
+}
---
+++ LMS.Application/LMS.Application/Features/Commands/Requests/Education/TermCourse/UpdateTermCourseRequest.cs
@@ -0,0 +1,11 @@
+﻿using LMS.Application.Dtos.Common;
+using LMS.Application.Dtos.Education;
+using MediatR;
+
+namespace LMS.Application.Features.Commands.Requests.Education.TermCourse
+{
+    public class UpdateTermCourseRequest : IRequest<BaseCommandResponse>
+    {
+        public required TermCourseDto TermCourseDto { get; set; }
+    }
+}
